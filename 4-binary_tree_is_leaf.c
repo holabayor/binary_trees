a5d0eaff2eb421362de74a8913e760cb871824b8
holabayor <liasu.olabayo@gmail.com>
2022-08-23 17:59:31 +0100

    prototype check

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,7 +5,7 @@
  * @node: pointer to the node to check
  * Return: 1 if node is a leaf else 0
  */
-int *binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node || node->left != NULL || node->right != NULL)
 		return (0);
